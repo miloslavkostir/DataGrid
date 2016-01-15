753a4807887721455e2495c786be1795f69093fb
Miloslav Koštíř <miloslav.kostir@gmail.com>
2016-01-15 15:07:28 +0100

    Last commit was wrong. Again : grid.ajax.js for AJAX - functional solution

--- assets/js/grid.ajax.js
+++ assets/js/grid.ajax.js
@@ -1,17 +1,97 @@
-$('.grid a.grid-ajax:not(.grid-confirm)').live('click', function (event) {
-	event.preventDefault();
-	$.get(this.href);
-});
+/**
+ * AJAX JS for DataGrid
+ * @author		Miloslav Kostir
+ * @dependence  jQuery 
+ *  
+ * Copied from holubj/NiftyGrid : 
+ * @author		Jakub Holub
+ * @copyright 	Copyright (c) 2012 Jakub Holub
+ * @license     New BSD Licence 
+ */ 
+  
+var GridAjax = {
 
-$('.grid a.grid-confirm:not(.grid-ajax)').live('click', function (event) {
-	var answer = confirm($(this).data("grid-confirm"));
-	return answer;
-});
+	init: function(ajax){
+		
+		// Setup
+		$.ajaxSetup({
+			success: function(data){
+				if(data.redirect){
+					$.get(data.redirect);
+				}
+				if(data.snippets){
+					for (var snippet in data.snippets){
+						$("#"+snippet).html(data.snippets[snippet]);
+					}
+				}
+			}
+		});
+	
+		// AJAX without confirmation
+		$('.grid a.grid-ajax:not(.grid-confirm)').on('click', function (event) {
+			event.preventDefault();
+			$.get(this.href);
+		});
+		
+		// Confirmation without AJAX	
+		$('.grid a.grid-confirm:not(.grid-ajax)').on('click', function (event) {
+			var answer = confirm($(this).data("grid-confirm"));
+			return answer;
+		});
+		
+		// AJAX with confirmation	
+		$('.grid a.grid-confirm.grid-ajax').on('click', function (event) {
+			event.preventDefault();
+			var answer = confirm($(this).data("grid-confirm"));
+			if (answer) {
+				$.get(this.href);
+			} 
+		}); 
 
-$('.grid a.grid-confirm.grid-ajax').live('click', function (event) {
-	event.preventDefault();
-	var answer = confirm($(this).data("grid-confirm"));
-	if (answer) {
-		$.get(this.href);
+		// Form AJAX
+		$(".grid-gridForm").find("*[type=submit]").on("click", function(){
+			$(this).addClass("grid-gridForm-clickedSubmit");
+		});
+	
+		$(".grid-gridForm").on("submit", function(event){
+			var button = $(".grid-gridForm-clickedSubmit");
+			$(button).removeClass("grid-gridForm-clickedSubmit");
+			if($(button).data("select")){
+				var selectName = $(button).data("select");
+				var option = $("select[name=\""+selectName+"\"] option:selected");
+				if($(option).data("grid-confirm")){
+					var answer = confirm($(option).data("grid-confirm"));
+					if(answer){
+						if($(option).hasClass("grid-ajax")){
+							event.preventDefault();
+							$.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
+						}
+					}else{
+						return false;
+					}
+				}else{
+					if($(option).hasClass("grid-ajax")){
+						event.preventDefault();
+						$.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
+					}
+				}
+			}else{
+				event.preventDefault();
+				$.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
+			}
+		});
+	
 	}
-});
+}
+
+$(function(){
+	
+	GridAjax.init(false);
+	  	
+	$(this).ajaxStop(function(){
+		GridAjax.init(true);
+	});
+	
+}); 
+
+
